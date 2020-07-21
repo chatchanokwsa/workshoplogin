345e5f4e0f4db1f0df72241a19f6378dc711988e
chatchanok.wsa <chatchanok.wongsamang@allianz.com>
2020-07-21 14:37:22 +0700

    product_app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
